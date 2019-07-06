87f2b4b6b53539cc699c8789b5ddd7ab5b809576
UjjwalGitter <ujjwalsinghgravity@gmail.com>
2019-07-06 13:28:58 +0530

    Product description layout created

--- app/src/main/java/com/freshokartz/ProAdapter.java
+++ app/src/main/java/com/freshokartz/ProAdapter.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,9 +76,10 @@ public class ProAdapter extends RecyclerView.Adapter<ProAdapter.ProAdapterViewHo
 
     public class ProAdapterViewHolder extends RecyclerView.ViewHolder {
         ExpandableRelativeLayout relativeLayout;
-
+        boolean invalidQuantity=false;
+        boolean checkOfUnknown;
         TextView textView, price;
-        int finalValue;
+        double finalValue;
         ImageView imageView, plus, minus, dropd;
         EditText incre;
         Button buyNow;
@@ -85,6 +88,7 @@ public class ProAdapter extends RecyclerView.Adapter<ProAdapter.ProAdapterViewHo
 
         public ProAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
+
             relativeLayout = itemView.findViewById(R.id.out);
             buyNow = itemView.findViewById(R.id.buynow);
             dropd = itemView.findViewById(R.id.dropd);
@@ -102,22 +106,77 @@ public class ProAdapter extends RecyclerView.Adapter<ProAdapter.ProAdapterViewHo
             });
             textView = itemView.findViewById(R.id.text);
             incre = itemView.findViewById(R.id.incre);
+
+            incre.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    try {
+                        double gtr = Double.parseDouble(s.toString());
+                        if (gtr > 10) {
+                            s.replace(0, s.length(), "10");
+                        }
+                    } catch (NumberFormatException e) {
+                    }
+
+                    try {
+                        double krt = Double.parseDouble(s.toString());
+                        String numberD = String.valueOf(krt);
+                        numberD = numberD.substring(numberD.indexOf("."));
+                        System.out.println(numberD);
+
+                        Log.i("numbers", numberD);
+                        if (!numberD.equals(".5") && !numberD.equals(".25") && !numberD.equals(".75") && !numberD.equals(".0") ) {
+                            Log.i("nbeers", numberD);
+                            incre.setError("Write quantity like 0.25,1, 1.5, 1.75 ",null);
+                            buyNow.setEnabled(false);
+                        }
+                        else {
+                            buyNow.setEnabled(true);
+                            //invalidQuantity=false;
+                            incre.setError(null);
+                        }
+                    } catch (Exception e) {
+                    }
+
+                }
+            });
+
             plus = itemView.findViewById(R.id.plus);
             minus = itemView.findViewById(R.id.minus);
             price = itemView.findViewById(R.id.price);
             imageView = itemView.findViewById(R.id.img_product);
             spinner = itemView.findViewById(R.id.spin);
-            String[] items = new String[]{"Kg", "g"};
+            String[] items = new String[]{"Kg"};
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(itemView.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
             spinner.setAdapter(adapter);
+
+
 
             plus.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String value = incre.getText().toString();
-                    finalValue = Integer.parseInt(value);
-                    finalValue++;
-                    incre.setText(String.valueOf(finalValue));
+                    try {
+                        finalValue = Double.parseDouble(value);
+                        double gra = (int) finalValue;
+                        if (!checkOfUnknown) {
+                            finalValue = gra;
+                        }
+                        finalValue = finalValue + 0.250;
+                        incre.setText(String.valueOf(finalValue));
+                        checkOfUnknown = true;
+                    } catch (Exception e) {
+                    }
                 }
             });
 
@@ -125,13 +184,46 @@ public class ProAdapter extends RecyclerView.Adapter<ProAdapter.ProAdapterViewHo
                 @Override
                 public void onClick(View v) {
                     String value = incre.getText().toString();
-                    finalValue = Integer.parseInt(value);
-                    finalValue--;
-                    if (finalValue > 0)
-                        incre.setText(String.valueOf(finalValue));
+                    try {
+                        finalValue = Double.parseDouble(value);
+                        double gra = (int) finalValue;
+                        if (!checkOfUnknown) {
+                            finalValue = gra;
+                            finalValue = finalValue + 0.250;
+                        }
+                        finalValue = finalValue - 0.250;
+                        if (finalValue > 0) {
+                            incre.setText(String.valueOf(finalValue));
+                        }
+                        checkOfUnknown = true;
+                    } catch (Exception e) {
+                    }
                 }
             });
+
+
+//            plus.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    String value = incre.getText().toString();
+//                    finalValue = Integer.parseInt(value);
+//                    finalValue++;
+//                    incre.setText(String.valueOf(finalValue));
+//                }
+//            });
+//
+//            minus.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    String value = incre.getText().toString();
+//                    finalValue = Integer.parseInt(value);
+//                    finalValue--;
+//                    if (finalValue > 0)
+//                        incre.setText(String.valueOf(finalValue));
+//                }
+//            });
         }
+
     }
 
 
